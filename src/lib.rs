115c6a1fec93b9bbf632e34beebf6c82c0f1c170
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2017-11-21 13:47:00 -0500

    placeholder

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,10 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
 pub struct Config {
     pub query: String,
     pub filename: String,
